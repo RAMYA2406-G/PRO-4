9d34e72b0d2b9dcc04bb2da796ee8e9f47b96654
RAMYA G <ramyadhara68@gmail.com>
2026-09-11 10:02:43 +0530

    Correct SELinux context and permissions in script
    Fix SELinux context assignment and permissions for web directory and HTML file.

--- student_solution.sh
+++ student_solution.sh
@@ -5,29 +5,31 @@
 # Register Number:
 
 echo "===== SELinux Status ====="
-
+sestatus
 
 echo "===== Creating Web Directory ====="
-
+mkdir -p /var/www/html/selinux
 echo "===== Creating HTML File ====="
+echo "SELinux Access denial Practical"> /var/www/selinux/index.html
 
 
 echo "===== Setting Linux Permissions ====="
-
-
+chmod 755 /var/www/html/selinux
+chmod 644 /var/www/html/selinux/index.html
 echo "===== Checking Initial Context ====="
 
 
 echo "===== Assigning Wrong SELinux Context ====="
-
+ls -Z /var/www/html/selinux/indext.html
 echo "===== Checking Wrong Context ====="
-
+chcon -t user_name_t /var/www/html/selinux/index.html
 
 echo "===== Checking AVC Denials ====="
+ausearch -m AVC -ts recent
 echo "===== Correcting SELinux Context ====="
-
+restorecon -v /var/www/html/selinux/index.html
 
 echo "===== Checking Correct Context ====="
-
+ls -Z /var/www/html/selinux/index.html
 
 echo "===== Practical Completed ====="
